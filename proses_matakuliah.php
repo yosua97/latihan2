f4a71f564364da9d7dd267d53076bf6d60d853f9
dianyosua <yosuamahulae@gmail.com>
2017-10-06 11:40:43 +0700

    mata kuliah

---
+++ proses_matakuliah.php
@@ -0,0 +1,23 @@
+<?php //filename: proses_mhs.php
+	include ("db.php");
+
+	if($_GET['action'] == "add")
+	{
+		//2. Query
+		$query ="INSERT INTO matakuliah (kode_matkul, nama_matkul) VALUES ('$_POST[kode_matkul]', '$_POST[nama_matkul]')";
+	}
+	else if($_GET['action'] == "edit")
+	{
+		//2.Query
+		$query = "UPDATE matakuliah SET kode_matkul = '$_POST[kode_matkul]', nama_matkul = '$_POST[nama_matkul]' WHERE id=$_POST[id]";
+	}
+	else if($_GET['action'] == "delete")
+	{
+		$query = "DELETE FROM matakuliah WHERE id =$_GET[id]";
+	}
+
+	mysqli_query ($koneksi, $query);
+	
+	//REDIRECT ke template.php
+	header ('Location: template.php?page=matakuliah');
+?>
