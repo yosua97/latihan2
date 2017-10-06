6a46e1da16c0ab2d0ca649e179ffbddf5002034a
dianyosua <yosuamahulae@gmail.com>
2017-10-06 11:37:00 +0700

    change

---
+++ dosen.php
@@ -0,0 +1,36 @@
+<?php
+	include("db.php");
+
+	$query = "SELECT * FROM dosen";
+	$hasil = mysqli_query($koneksi, $query);
+?>
+
+<h1>Data Dosen</h1>
+<a href="template.php?page=formdosen&action=add">Tambah Data</a>
+<table>
+	<thead>
+		<tr>
+			<th>No.</th>
+			<th>Kode Dosen</th>
+			<th>Nama Dosen</th>
+			<th>Action</th>
+		</tr>
+	</thead>
+	<tbody>
+		<?php
+			while($row = mysqli_fetch_assoc($hasil)){
+		?>
+		<tr>
+			<td><?php echo $row['id']; ?></td>
+			<td><?php echo $row['kode_dosen']; ?></td>
+			<td><?php echo $row['nama']; ?></td>
+			<td>
+				<a href="template.php?page=formdosen&id= <?php echo $row['id'];?> &action=edit">Edit</a>
+				<a href="proses_dosen.php?action=delete&id= <?php echo $row['id'];?> ">Delete</a>
+			</td>
+		</tr>
+		<?php
+			}
+		?>
+	</tbody>
+</table>
